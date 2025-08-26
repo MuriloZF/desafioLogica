f0c5fc5b07839be62ffad97d63ebd6e6b2753742
Murilo Zimerman Fortaleza <mzimermanfortaleza@gmail.com>
2025-08-26 09:06:31 -0300

    Testing some stuff

--- logic.js
+++ logic.js
@@ -1,1 +1,20 @@
+function input(){
+	const textarea = document.querySelector("textarea");
+	const input = textarea.value;
+	const fnc = document.getElementById("FNC");
+	fnc.textContent = input;
+}
+
+function fnc(){}
+
+function fdc(){}
+
+
+document.addEventListener("DOMContentLoaded", () => {
+    const submit = document.querySelector("input[type='submit']");
+    submit.addEventListener("click", (event) => {
+        event.preventDefault();
+        input();
+    });
+});
 
