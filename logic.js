151915c0e2508f8bb5af52efee420b83e7a26543
Murilo Zimerman Fortaleza <mzimermanfortaleza@gmail.com>
2025-08-27 08:57:46 -0300

    Added latex support

--- logic.js
+++ logic.js
@@ -1,8 +1,21 @@
-function input(){
+function inputFunction(){
 	const textarea = document.querySelector("textarea");
-	const input = textarea.value;
+	const inputValue = textarea.value;
 	const fnc = document.getElementById("FNC");
-	fnc.textContent = input;
+	const fdc = document.getElementById("FDC");
+	fnc.textContent = inputValue;
+	MathJax.typesetPromise([fnc]);
+	fdc.textContent = inputValue;
+	fdc.textContent = inputValue;
+	MathJax.typesetPromise([fdc]);
+}
+
+function clearFunction(){
+	const fnc = document.getElementById("FNC");
+	const fdc = document.getElementById("FDC");
+	document.getElementById("form").reset();
+	fnc.textContent = "";
+	fdc.textContent = "";
 }
 
 function fnc(){}
@@ -14,7 +27,12 @@ document.addEventListener("DOMContentLoaded", () => {
     const submit = document.querySelector("input[type='submit']");
     submit.addEventListener("click", (event) => {
         event.preventDefault();
-        input();
+        inputFunction();
+    });
+    const clearButton = document.querySelector("button");
+    clearButton.addEventListener("click", (event) => {
+	   	event.preventDefault();
+		clearFunction();
     });
 });
 
